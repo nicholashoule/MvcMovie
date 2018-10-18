4509aa0a62e8d75d83e7db486b504da4abe9affc
Nicholas Houle <181gaming@gmail.com>
2018-10-18 17:29:55 -0600

    Get started with ASP.NET Core MVC and Visual Studio

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        // Params: string name, int numTimes
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
